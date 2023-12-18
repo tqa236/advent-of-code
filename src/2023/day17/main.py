86724cff42ae7afa1a4d8f372e911ea6d62e65b1
Anh Trinh <trinhquocanh94@gmail.com>
2023-12-18 19:29:11 +0100

    solve day 17

--- src/2023/day17/main.py
+++ src/2023/day17/main.py
@@ -1,133 +1,55 @@
-from enum import Enum
 from heapq import heappop, heappush
+from math import inf
 
-MIN_DISTANCE = 1e9
-
-
-class Direction(Enum):
-    NORTH = (-1, 0)
-    EAST = (0, 1)
-    SOUTH = (1, 0)
-    WEST = (0, -1)
-
-    def turn_clockwise(self):
-        new_direction = Direction((self.value[1], -self.value[0]))
-        return new_direction
-
-    def turn_counter_clockwise(self):
-        new_direction = Direction((-self.value[1], self.value[0]))
-        return new_direction
+LEGAL_MOVES = {
+    (0, 0): ((1, 0), (0, 1)),
+    (0, -1): ((1, 0), (-1, 0)),
+    (1, 0): ((0, -1), (0, 1)),
+    (0, 1): ((1, 0), (-1, 0)),
+    (-1, 0): ((0, -1), (0, 1)),
+}
 
 
-class Graph:
-    def __init__(self, matrix):
-        self.matrix = matrix
-        self.row = len(matrix)
-        self.column = len(matrix[0])
-        self.positions = [(i, j) for i in range(self.row) for j in range(self.column)]
-        self.visited = set()
-        self.distance = {
-            position: {
-                direction: [MIN_DISTANCE, MIN_DISTANCE, MIN_DISTANCE]
-                for direction in Direction
-            }
-            for position in self.positions
-        }
+def traverse(matrix: str, mmin: int, mmax: int):
+    destination_coord = (len(matrix[0]) - 1, len(matrix) - 1)
+    heap = [(0, (0, 0), (0, 0))]
+    heat_map = {(0, 0): 0}
+    visited = set()
 
-    def get_neighbor(self, position):
-        neighbors = []
-        if not position[0] < 1:
-            neighbors.append((position[0] - 1, position[1]))
-        if not position[0] >= self.row - 1:
-            neighbors.append((position[0] + 1, position[1]))
-        if not position[1] < 1:
-            neighbors.append((position[0], position[1] - 1))
-        if not position[0] >= self.column - 1:
-            neighbors.append((position[0], position[1] + 1))
-        return neighbors
+    while heap:
+        heat_loss, coord, direction = heappop(heap)
 
-    def get_direction(self, position):
-        directions = []
-        if not position[0] < 1:
-            directions.append(Direction.NORTH)
-        if not position[0] >= self.row - 1:
-            directions.append(Direction.SOUTH)
-        if not position[1] < 1:
-            directions.append(Direction.WEST)
-        if not position[1] >= self.column - 1:
-            directions.append(Direction.EAST)
-        return set(directions)
+        if coord == destination_coord:
+            break
 
-    def get_valid_move(self, position, direction, step):
-        valid_directions_from_direction = set(
-            [
-                direction,
-                direction.turn_clockwise(),
-                direction.turn_counter_clockwise(),
-            ]
-        )
-        valid_directions_from_position = self.get_direction(position)
-        valid_directions = valid_directions_from_direction.intersection(
-            valid_directions_from_position
-        )
-        if step == 2 and direction in valid_directions:
-            valid_directions.remove(direction)
-        return valid_directions
+        if (coord, direction) in visited:
+            continue
 
-    def get_min_cost(self, position, step, new_direction):
-        distance = self.distance[position]
-        # print("get_min_cost")
-        # print(distance)
-        # print(position, step, new_direction)
-        cost_same_direction = MIN_DISTANCE
-        if new_direction is not None:
-            cost_same_direction = min(
-                distance[new_direction][step + 1 :] + [MIN_DISTANCE]
-            )
-        cost_different_direction = min(
-            min(distance[d]) for d in Direction if d != new_direction
-        )
-        return min(cost_same_direction, cost_different_direction)
+        visited.add((coord, direction))
 
-    def update_cost(self, position, new_position, new_direction):
-        for d in Direction:
-            pass
+        for new_direction in LEGAL_MOVES[direction]:
+            new_heat_loss = heat_loss
+            for steps in range(1, mmax + 1):
+                new_coord = (
+                    coord[0] + steps * new_direction[0],
+                    coord[1] + steps * new_direction[1],
+                )
+                if (
+                    new_coord[0] < 0
+                    or new_coord[1] < 0
+                    or new_coord[0] > destination_coord[0]
+                    or new_coord[1] > destination_coord[1]
+                ):
+                    continue
+                new_heat_loss = new_heat_loss + matrix[new_coord[1]][new_coord[0]]
+                if steps >= mmin:
+                    new_node = (new_coord, new_direction)
+                    if heat_map.get(new_node, inf) <= new_heat_loss:
+                        continue
+                    heat_map[new_node] = new_heat_loss
+                    heappush(heap, (new_heat_loss, new_coord, new_direction))
 
-    def traverse(self, start):
-        self.distance[start] = {direction: [0, 0, 0] for direction in Direction}
-        directions = self.get_direction(start)
-        queue = [(start, direction, 0) for direction in directions]
-        while queue:
-            position, direction, step = queue.pop(0)
-            print(position, direction, step)
-            valid_directions = self.get_valid_move(position, direction, step)
-            if step == 2:
-                print(valid_directions)
-            for new_direction in valid_directions:
-                new_position = move(position, new_direction)
-                add_new_position = False
-                cost = self.get_min_cost(position, step, new_direction)
-                new_cost = cost + self.matrix[new_position[0]][new_position[1]]
-                for d in Direction:
-                    if d == new_direction:
-                        for s in range(step + 1, 3):
-                            if s < 0 or s >= 3:
-                                continue
-                            if new_cost < self.distance[new_position][d][s]:
-                                self.distance[new_position][d][s] = new_cost
-                                add_new_position = True
-                    else:
-                        for s in range(3):
-                            if new_cost < self.distance[new_position][d][s]:
-                                self.distance[new_position][d][s] = new_cost
-                                add_new_position = True
-                if add_new_position:
-                    new_step = step + 1 if new_direction == direction else 0
-                    queue.append((new_position, new_direction, new_step))
-
-
-def move(position, direction):
-    return position[0] + direction.value[0], position[1] + direction.value[1]
+    return heat_loss
 
 
 def read_input(file_path):
@@ -142,18 +64,13 @@ def parse_input(content):
 def get_result(case: str, file_path: str):
     content = read_input(file_path)
     matrix = parse_input(content)
-    graph = Graph(matrix)
-    # print(graph.row, graph.column)
-    graph.traverse((0, 0))
-    for key, value in graph.distance.items():
-        print(key)
-        print(value)
-    return graph.get_min_cost((graph.row - 1, graph.column - 1), None, None)
+    mmin, mmax = (1, 3) if case == "first" else (4, 10)
+    return traverse(matrix, mmin, mmax)
 
 
 if __name__ == "__main__":
-    # print(get_result("first", "data/2023/day17/sample.txt"))
+    print(get_result("first", "data/2023/day17/sample.txt"))
     print(get_result("first", "data/2023/day17/sample2.txt"))
-    # print(get_result("first", "data/2023/day17/input1.txt"))
-    # print(get_result("second", "data/2023/day17/sample.txt"))
-    # print(get_result("second", "data/2023/day17/input1.txt"))
+    print(get_result("first", "data/2023/day17/input1.txt"))
+    print(get_result("second", "data/2023/day17/sample.txt"))
+    print(get_result("second", "data/2023/day17/input1.txt"))
